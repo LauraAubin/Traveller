8f95d39b157b58f2be5a8ab473db1a45f5cd50be
Laura Aubin <laura.aubin@shopify.com>
2018-05-16 15:28:33 -0400

    Create Traveller class and make_general_api_call method

--- oc-transpo-gps-bus-tracker.rb
+++ oc-transpo-gps-bus-tracker.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/ruby
 
-puts "Hello Laura"
+require 'open-uri'
+require 'json'
+
+APP_ID = ""
+APP_KEY = ""
+
+class Traveller
+  def make_general_api_call (
+    http_methods = "GetRouteSummaryForStop",
+    route_number = "1",
+    stop_number = "1000" )
+
+    open("https://api.octranspo1.com/v1.2/#{http_methods}?appID=#{APP_ID}&apiKey=#{APP_KEY}&routeNo=#{route_number}&stopNo=#{stop_number}")
+  end
+end
+
+traveller = Traveller.new
+
+puts traveller.make_general_api_call
